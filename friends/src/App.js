c632c9618c9fbc6ec89c004e3f48175691524a15
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2019-06-25 21:38:22 -0400

    formatting

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,35 +1,41 @@
-import React from 'react';
-import './App.css';
-import axios from 'axios';
-import {Route, Link} from 'react-router-dom';
-import FriendsList from './components/FriendsList';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import { Route, Link } from "react-router-dom";
+import FriendsList from "./components/FriendsList";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-        friends: []
-    }
+      friends: []
+    };
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/friends')
-        .then(response => {
-            this.setState({ friends: response.data })
-        })
-        .catch(err => {
-            console.log('Error: ', err)
-        })
+    axios
+      .get("http://localhost:5000/friends")
+      .then(response => {
+        this.setState({ friends: response.data });
+      })
+      .catch(err => {
+        console.log("Error: ", err);
+      });
   }
 
   render() {
     return (
       <div className="App">
-        <Route path = '/' render = {(props) => <FriendsList {...props} friends = {this.state.friends}/>} />
+        <Route
+          path="/"
+          render={props => (
+            <FriendsList {...props} friends={this.state.friends} />
+          )}
+        />
       </div>
     );
-    }
+  }
 }
 
 export default App;
